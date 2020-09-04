ed2277b7627faeb770584e823714e805e4acf0f3
muradi123 <36930686+muradi123@users.noreply.github.com>
2020-09-04 23:11:56 +0400

    update code

--- scripts/background.js
+++ scripts/background.js
@@ -1,23 +1,19 @@
-
-
-
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+  function(request, sender, sendResponse) {
 
-      if (request.greeting){
-        for (i = 0; i < request.leng; i++) {
-            var nameItem = {
-                "id": `${i}`,
-                "title": "Save as Name",
-                "contexts": ["selection"]
-            };
-           
-            console.log(request.greeting)
-            chrome.contextMenus.create(nameItem);
-          }
-       
-      }
+    if (request.greeting){
       
-    });
-
+          var nameItem = {
+              "id": `${request.leng}`,
+              "title": `${request.greeting}`,
+              "contexts": ["selection"]
+          };
+         
+          console.log(request.greeting)
+          chrome.contextMenus.create(nameItem);
    
+     
+    }
+    
+  });
+
--- scripts/option.js
+++ scripts/option.js
@@ -1,126 +1,266 @@
-  $( function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-  });
+let index;
+$(".zimpvyvzds").click(function () {
+ index = $(this).parent().parent().parent().parent().index();
+ $(this).addClass('uuuhuzxnpd');
+ $('.dvbakbohhy').eq(index).addClass('dgsrbipams');
+ console.log(index);
 
- 
-  $(".fwiqxrwsvl").click(function () {
-    $(this).parent().parent().parent().parent().remove();
-  });
- 
-   
+ if (index >= 0) {
+  $('.jylzvkgxxk').eq(index).addClass( "xfunloppbp" );
+  $('.cxvlaxyzlh').eq(index).addClass( "scptwsygwd" );
+  $('.rpyeyrjzqb').eq(index).addClass( "xfunloppbp" );
+  $('.kxbqmvmapu').eq(index).addClass( "scptwsygwd" );
+}
+});
+let index2;
+$('.jylzvkgxxk').on('click', function() {
+  index2 = $(this).parent().parent().parent().parent().parent().parent().index();
+  console.log(index2);
+  if($(this).hasClass('xfunloppbp')){
+    $('.miyhwhxnaj').eq(index2).toggleClass("denjnyqajo");
+  }
+});
+$( function() {
+$( "#sortable" ).sortable();
+$( "#sortable" ).disableSelection();
+});
+
+$(".fwiqxrwsvl").click(function () {
+  $(this).parent().parent().parent().parent().remove();
+    let allU = document.querySelector('.ui-sortable');
+     let ts = JSON.stringify(allU.outerHTML);
+     chrome.storage.local.set({objVal:ts});
+
+});
+
+let index3;
+
+
+$('.dvbakbohhy').click(function() {
+
+index3 =  $(this).parent().parent().parent().parent().index();
+$('.dvbakbohhy').eq(index3).removeClass('dgsrbipams');
+$('.zimpvyvzds').eq(index3).removeClass('uuuhuzxnpd');
+console.log(index3)
+if (index >= 0) {
+$('.jylzvkgxxk').eq(index3).removeClass( "xfunloppbp" );
+$('.cxvlaxyzlh').eq(index3).removeClass( "scptwsygwd" );
+$('.rpyeyrjzqb').eq(index3).removeClass( "xfunloppbp" );
+$('.kxbqmvmapu').eq(index3).removeClass( "scptwsygwd" );
+}
+let allU = document.querySelector('.ui-sortable');
+let ts = JSON.stringify(allU.outerHTML);
+chrome.storage.local.set({objVal:ts});
+});
+
+$(".rpyeyrjzqb").click(function () {
+index2 = $(this).parent().parent().parent().parent().parent().parent().index();
+if($(this).hasClass('xfunloppbp')){
+$('.dvzowjifyb').eq(index2).toggleClass("denjnyqajo");
+
+}
+});
+
   
-   let index2;
-   $('.jylzvkgxxk').on('click', function() {
+let allUl = document.querySelectorAll('.ui-state-default');
+for(let i = 0; i < allUl.length ; i ++) { 
 
+const from_currencyEl = document.querySelectorAll('.alaepzlpla')[i];
+const from_ammountEl = document.getElementById('from_ammount');
+const to_currencyEl = document.querySelectorAll('.xwswmowfii')[i];
+const to_ammountEl = document.getElementById('to_ammount');
+const rateEl = document.getElementById('rate');
+let fullUL = document.querySelector('.rfwfw');
+console.log(i);
+function calculate() {
+const from_currency = from_currencyEl.innerHTML;
+const to_currency = to_currencyEl.innerHTML;
+
+fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`)
+  .then(res => res.json())
+  .then(res => {
+  const rate = res.rates[to_currency];
+  let currency =   rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`;
+  fullUL.innerHTML += '<div class="curr">' + currency + '</div>';
+  
+  to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
+  chrome.runtime.sendMessage({greeting: currency, leng:i})
+})
+}
+calculate();
+from_currencyEl.addEventListener('change', function(){
+console.log('woohoo')
+})
+}
+$(".miyhwhxnaj li input").click(function () {
+let value =  $( this ).val();
+$('.alaepzlpla').eq(index2).text(value);
+$('.gbmghponmn').eq(index2).attr(`src`, `../images/${value}.png`);
+$(this).parent().parent().removeClass('denjnyqajo') 
+});
+$(".dvzowjifyb li input").click(function () {
+let value =  $( this ).val();
+$('.xwswmowfii').eq(index2).text(value);
+$('.ywnifddqjx').eq(index2).attr(`src`, `../images/${value}.png`);
+$(this).parent().parent().removeClass('denjnyqajo')
+});
+
+
+chrome.storage.local.get(['objVal'], function(result) {
+    document.querySelector('.gyfpyxmhmcpllye').innerHTML = '<div class="dgnownlamlcnsjj"><div class="ugpvlpxayggfdln"></div><div class="sekbvoujtoswvbd"><span class="cmwnyoemtinmmaf">Base currency</span></div><div class="prycpqedgjhoqdb">Convert to</div><div class="hyxjejbqjtfkplr"></div></div>' + '<div>'+ JSON.parse(result.objVal) + '</div>';
+    let index;
+    $(".zimpvyvzds").click(function () {
+     index = $(this).parent().parent().parent().parent().index();
+     $(this).addClass('uuuhuzxnpd');
+     $('.dvbakbohhy').eq(index).addClass('dgsrbipams');
+     console.log(index);
+    
+     if (index >= 0) {
+      $('.jylzvkgxxk').eq(index).addClass( "xfunloppbp" );
+      $('.cxvlaxyzlh').eq(index).addClass( "scptwsygwd" );
+      $('.rpyeyrjzqb').eq(index).addClass( "xfunloppbp" );
+      $('.kxbqmvmapu').eq(index).addClass( "scptwsygwd" );
+    }
+  });
+  let index2;
+   $('.jylzvkgxxk').on('click', function() {
       index2 = $(this).parent().parent().parent().parent().parent().parent().index();
       console.log(index2);
       if($(this).hasClass('xfunloppbp')){
         $('.miyhwhxnaj').eq(index2).toggleClass("denjnyqajo");
-       
       }
-      
-      
-  
 });
-   
-   
-   let index;
-   $(".zimpvyvzds").click(function () {
-    index = $(this).parent().parent().parent().parent().index();
-    $(this).addClass('uuuhuzxnpd');
-    $('.dvbakbohhy').eq(index).addClass('dgsrbipams');
-    console.log(index);
-   
-    if (index >= 0) {
-     $('.jylzvkgxxk').eq(index).addClass( "xfunloppbp" );
-     $('.cxvlaxyzlh').eq(index).addClass( "scptwsygwd" );
-     $('.rpyeyrjzqb').eq(index).addClass( "xfunloppbp" );
-     $('.kxbqmvmapu').eq(index).addClass( "scptwsygwd" );
-   }
- 
- });
+$( function() {
+  $( "#sortable" ).sortable();
+  $( "#sortable" ).disableSelection();
+});
+
+$(".fwiqxrwsvl").click(function () {
+  $(this).parent().parent().parent().parent().remove();
+    let allU = document.querySelector('.ui-sortable');
+     let ts = JSON.stringify(allU.outerHTML);
+     chrome.storage.local.set({objVal:ts});
+
+});
+
+let index3;
 
 
-setTimeout(function(){  var obj = [];
-  let allUl = document.querySelectorAll('.ui-state-default');
-  for(let i = 0; i < allUl.length ; i ++) {
-   let firsthtml =  $('.alaepzlpla').eq(i).html();
-   let secondthtml =  $('.xwswmowfii').eq(i).html();
-   obj.push([firsthtml, secondthtml]);
-   chrome.storage.local.set({objVal: obj});
-   console.log(obj);
- }; }, 5000);
+$('.dvbakbohhy').click(function() {
+  let hiuo = document.querySelector('.rfwfw');
+  let ksk = JSON.stringify(hiuo.outerHTML)
+        console.log(ksk)
+       chrome.storage.local.set({ul: ksk});
+   
 
- setTimeout(function(){ 
-  chrome.storage.local.get(['objVal'], function(result) {
-    let valob = result.objVal;
-    let allUl = document.querySelectorAll('.ui-state-default');
-   for(let i = 0; i < allUl.length ; i ++) {
-    let test = valob[i];
-    $('.alaepzlpla').eq(i).text(test[1]);
-    $('.gbmghponmn').eq(i).attr(`src`, `../images/${test[1]}.png`);
-    console.log(test[1]);
-   }
-  
-   
-   
-  
+   index3 =  $(this).parent().parent().parent().parent().index();
+    $('.dvbakbohhy').eq(index3).removeClass('dgsrbipams');
+    $('.zimpvyvzds').eq(index3).removeClass('uuuhuzxnpd');
+    console.log(index3)
+   if (index >= 0) {
+    $('.jylzvkgxxk').eq(index3).removeClass( "xfunloppbp" );
+    $('.cxvlaxyzlh').eq(index3).removeClass( "scptwsygwd" );
+    $('.rpyeyrjzqb').eq(index3).removeClass( "xfunloppbp" );
+    $('.kxbqmvmapu').eq(index3).removeClass( "scptwsygwd" );
+  }
+  let allU = document.querySelector('.ui-sortable');
+  let ts = JSON.stringify(allU.outerHTML);
+  chrome.storage.local.set({objVal:ts});
   });
-  }, 5000);
+ 
+ $(".rpyeyrjzqb").click(function () {
+  index2 = $(this).parent().parent().parent().parent().parent().parent().index();
+  if($(this).hasClass('xfunloppbp')){
+   $('.dvzowjifyb').eq(index2).toggleClass("denjnyqajo");
+  
+ }
+});
 
+$(".miyhwhxnaj li input").click(function () {
+  let value =  $( this ).val();
+  $('.alaepzlpla').eq(index2).text(value);
+  $('.gbmghponmn').eq(index2).attr(`src`, `../images/${value}.png`);
+  $(this).parent().parent().removeClass('denjnyqajo') 
+});
+$(".dvzowjifyb li input").click(function () {
+  let value =  $( this ).val();
+  $('.xwswmowfii').eq(index2).text(value);
+  $('.ywnifddqjx').eq(index2).attr(`src`, `../images/${value}.png`);
+  $(this).parent().parent().removeClass('denjnyqajo')
+});
 
- let index3;
-    $( ".dvbakbohhy" ).click(function() {
-      index3 =  $(this).parent().parent().parent().parent().index();
-     $('.dvbakbohhy').eq(index3).removeClass('dgsrbipams');
-     $('.zimpvyvzds').eq(index3).removeClass('uuuhuzxnpd');
-     console.log(index3)
+let allUl = document.querySelectorAll('.ui-state-default');
+for(let i = 0; i < allUl.length ; i ++) { 
 
+    const from_currencyEl = document.querySelectorAll('.alaepzlpla')[i];
+    const from_ammountEl = document.getElementById('from_ammount');
+    const to_currencyEl = document.querySelectorAll('.xwswmowfii')[i];
+    const to_ammountEl = document.getElementById('to_ammount');
+    const rateEl = document.getElementById('rate');
+    let fullUL = document.querySelector('.rfwfw');
+    console.log(i);
+    function calculate() {
+    const from_currency = from_currencyEl.innerHTML;
+    const to_currency = to_currencyEl.innerHTML;
+    
+    fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`)
+      .then(res => res.json())
+      .then(res => {
+      const rate = res.rates[to_currency];
+      let currency =   rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`;
+      fullUL.innerHTML += '<div class="curr">' + currency + '</div>';
+      to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
+      chrome.runtime.sendMessage({greeting: currency, leng:i})
+    })
+  }
+  calculate();
+  from_currencyEl.addEventListener('change', function(){
+    console.log('woohoo')
+  })
+}
+  });
+ 
+ 
+  chrome.storage.onChanged.addListener(changes => {
+    if (changes.objVal) {
+  
+
+      let allUl = document.querySelectorAll('.ui-state-default');
+      for(let i = 0; i < allUl.length ; i ++) { 
       
+          const from_currencyEl = document.querySelectorAll('.alaepzlpla')[i];
+          const from_ammountEl = document.getElementById('from_ammount');
+          const to_currencyEl = document.querySelectorAll('.xwswmowfii')[i];
+          const to_ammountEl = document.getElementById('to_ammount');
+          const rateEl = document.getElementById('rate');
+          let fullUL = document.querySelector('.rfwfw');
+          console.log(i);
+          function calculate() {
+          const from_currency = from_currencyEl.innerHTML;
+          const to_currency = to_currencyEl.innerHTML;
+          
+          fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`)
+            .then(res => res.json())
+            .then(res => {
+            const rate = res.rates[to_currency];
+            let currency =   rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`;
+            fullUL.innerHTML += '<div class="curr">' + currency + '</div>';
+            to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
+            chrome.runtime.sendMessage({greeting: currency, leng:i})
+          })
+        }
+        calculate();
+        from_currencyEl.addEventListener('change', function(){
+          console.log('woohoo')
+        })
+      }
 
-  
-     
-    if (index >= 0) {
-     $('.jylzvkgxxk').eq(index3).removeClass( "xfunloppbp" );
-     $('.cxvlaxyzlh').eq(index3).removeClass( "scptwsygwd" );
-     $('.rpyeyrjzqb').eq(index3).removeClass( "xfunloppbp" );
-     $('.kxbqmvmapu').eq(index3).removeClass( "scptwsygwd" );
-   }
 
-   
-  
-  
-   });
- 
-
-
-   $(".rpyeyrjzqb").click(function () {
-     index2 = $(this).parent().parent().parent().parent().parent().parent().index();
-     if($(this).hasClass('xfunloppbp')){
-      $('.dvzowjifyb').eq(index2).toggleClass("denjnyqajo");
-     
     }
-   });
-  
- 
- 
-   $(".miyhwhxnaj li input").click(function () {
-     let value =  $( this ).val();
-     $('.alaepzlpla').eq(index2).text(value);
-     $('.gbmghponmn').eq(index2).attr(`src`, `../images/${value}.png`);
-     $(this).parent().parent().removeClass('denjnyqajo') 
-   });
- 
- 
- 
-   $(".dvzowjifyb li input").click(function () {
-     let value =  $( this ).val();
-     $('.xwswmowfii').eq(index2).text(value);
-     $('.ywnifddqjx').eq(index2).attr(`src`, `../images/${value}.png`);
-     $(this).parent().parent().removeClass('denjnyqajo')
-   });
-  
+  });
+
+
+
   let div = `
   <li class="ui-state-default ui-sortable-handle">
                        <div class="nnspczfzpudilqu">
@@ -253,7 +393,7 @@ setTimeout(function(){  var obj = [];
                            <div class="nuertaryteqckdc">
                                <div class="wruzmkqwvu">
                                    <div class="ontabbcmca">Edit</div>
-                                   <div class="dvbakbohhy"><span class="djttbbjuyc">Save</spa></div>
+                                   <div class="dvbakbohhy2"><span class="djttbbjuyc">Save</spa></div>
                                    <div class="fwiqxrwsvl">Delete</div>
                                </div>
                            </div>
@@ -264,11 +404,37 @@ setTimeout(function(){  var obj = [];
  
   $( ".fubospzpgh" ).click(function() {
    $('.ui-sortable').append(div)
-   
+   let allU = document.querySelector('.ui-sortable');
+   let ts = JSON.stringify(allU.outerHTML);
+   chrome.storage.local.set({objVal:ts});
+
+
+
    $(".fwiqxrwsvl").click(function () {
     $(this).parent().parent().parent().parent().remove();
+    $(".fwiqxrwsvl").click(function () {
+      $(this).parent().parent().parent().parent().remove();
+      let allU = document.querySelector('.ui-sortable');
+       let ts = JSON.stringify(allU.outerHTML);
+       chrome.storage.local.set({objVal:ts});
+    });
+     
   });
-   
+
+  $('.lpmczgnbnr').on('click', function() {
+    index2 = $(this).parent().parent().parent().parent().parent().parent().index();
+    console.log(index2);
+    if($(this).hasClass('xfunloppbp')){
+      $('.miyhwhxnaj').eq(index2).toggleClass("denjnyqajo");
+    }
+  });
+  $( function() {
+  $( "#sortable" ).sortable();
+  $( "#sortable" ).disableSelection();
+  });
+  
+  
+
   let index;
   $(".ontabbcmca").click(function () {
    index = $(this).parent().parent().parent().parent().index();
@@ -287,42 +453,23 @@ setTimeout(function(){  var obj = [];
 
 });
 
-setTimeout(function(){
-  let allUl = document.querySelectorAll('.ui-state-default');
-  for(let i = 0; i < allUl.length ; i ++) {
-      
-  const from_currencyEl = document.querySelectorAll('.alaepzlpla')[i];
-  console.log(from_currencyEl)
-  const from_ammountEl = document.getElementById('from_ammount');
-  const to_currencyEl = document.querySelectorAll('.xwswmowfii')[i];
-  console.log(to_currencyEl)
-  const to_ammountEl = document.getElementById('to_ammount');
-  const rateEl = document.getElementById('rate');
-  let fullUL = document.querySelector('.rfwfw');
-  
-console.log(i);
-function calculate() {
-	const from_currency = from_currencyEl.innerHTML;
-	const to_currency = to_currencyEl.innerHTML;
-	
-	fetch(`https://api.exchangerate-api.com/v4/latest/${from_currency}`)
-		.then(res => res.json())
-		.then(res => {
-		const rate = res.rates[to_currency];
-   let currency = 	rateEl.innerText = `1 ${from_currency} = ${rate} ${to_currency}`;
-   let string =  fullUL.innerHTML += '<div>' + currency + '</div>';
-   let putTosting = JSON.stringify(string);
-   console.log(putTosting);
-    to_ammountEl.value = (from_ammountEl.value * rate).toFixed(2);
-    chrome.runtime.sendMessage({greeting: string, leng: allUl.length})
-    
-	})
-}
- 
-calculate();
-     
-}
-}, 2000);
+$('.dvbakbohhy2').click(function() {
+
+  index3 =  $(this).parent().parent().parent().parent().index();
+  $('.dvbakbohhy2').eq(index3).removeClass('dgsrbipams');
+  $('.zimpvyvzds').eq(index3).removeClass('uuuhuzxnpd');
+  console.log(index3)
+  if (index >= 0) {
+  $('.jylzvkgxxk').eq(index3).removeClass( "xfunloppbp" );
+  $('.cxvlaxyzlh').eq(index3).removeClass( "scptwsygwd" );
+  $('.rpyeyrjzqb').eq(index3).removeClass( "xfunloppbp" );
+  $('.kxbqmvmapu').eq(index3).removeClass( "scptwsygwd" );
+  }
+  let allU = document.querySelector('.ui-sortable');
+  let ts = JSON.stringify(allU.outerHTML);
+  chrome.storage.local.set({objVal:ts});
+  });
+
 
 $( function() {
   $( "#sortable" ).sortable({
@@ -366,7 +513,11 @@ $( function() {
       }
       }, 2000);
       $(this).data('idx', idx);
+   
     }
+     
   });
   $( "#sortable" ).disableSelection();
 });
+
+
